99e2ba056b43b1111bf34f792eef2c9ce53a36a7
Robert Elwell <robert@wikia-inc.com>
2013-12-18 09:25:31 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="wikia_authority",
+    version= "0.0.1",
+    author = "Robert Elwell",
+    author_email = "robert@wikia-inc.com",
+    description = "Library for identifying authorship quality in a revision-based system",
+    license = "Other",
+    packages = ["wikia_authority"],
+    depends = [""]
+    )
