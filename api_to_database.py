563e8d52dab357d045d18da776782c47171435f4
Robert Elwell <robert@wikia-inc.com>
2014-01-23 13:53:41 -0800

    python one is working as it should

--- api_to_database.py
+++ api_to_database.py
@@ -12,6 +12,22 @@ edit_distance_memoization_cache = {}
 test_run = False
 
 
+class MinMaxScaler:
+    """
+    Scales values from 0 to 1 by default
+    """
+
+    def __init__(self, vals, enforced_min=0, enforced_max=1):
+        self.min = min(vals)
+        self.max = max(vals)
+        self.enforced_min = enforced_min
+        self.enforced_max = enforced_max
+
+    def scale(self, val):
+        return (((self.enforced_max - self.enforced_min) * (val - self.min))
+                / (self.max - self.min)) + self.enforced_min
+
+
 def get_all_titles(apfrom=None, aplimit=500):
     global api_url, test_run
     params = {'action': 'query', 'list': 'allpages', 'aplimit': aplimit,
@@ -42,10 +58,10 @@ def get_all_revisions(title_object, rvstartid=None):
     if 'query-continue' in response:
         return (title_string, (revisions
                 + get_all_revisions(title_object, rvstartid=response['query-continue']['revisions']['rvstartid'])[1]))
-    return title_string, revisions
+    return [title_string, revisions]
 
 
-def edit_distance(title, earlier_revision, later_revision):
+def edit_distance(title_object, earlier_revision, later_revision):
     global api_url, edit_distance_memoization_cache
     if (later_revision, earlier_revision) in edit_distance_memoization_cache:
         return edit_distance_memoization_cache[(earlier_revision, later_revision)]
@@ -56,10 +72,15 @@ def edit_distance(title, earlier_revision, later_revision):
               'format': 'json',
               'rvstartid': earlier_revision,
               'rvdiffto': later_revision,
-              'titles': title}
+              'titles': title_object['title']}
+
     resp = requests.get(api_url, params=params)
     response = resp.json()
-    revision = response.get('query', {}).get('pages', {0: {}}).values()[0].get('revisions', [])[0]
+
+    revision = (response.get('query', {})
+                        .get('pages', {0: {}})
+                        .get(unicode(title_object['pageid']), {})
+                        .get('revisions', [{}])[0])
     revision['adds'], revision['deletes'], revision['moves'] = 0, 0, 0
     if ('diff' in revision and '*' in revision['diff']
        and revision['diff']['*'] != '' and revision['diff']['*'] is not False and revision['diff']['*'] is not None):
@@ -79,62 +100,68 @@ def edit_distance(title, earlier_revision, later_revision):
             moves /= changes
         distance = max([adds, deletes]) - 0.5 * min([adds, deletes]) + moves
         edit_distance_memoization_cache[(earlier_revision, later_revision)] = distance
-        # normalization
-        distance = distance if distance >= -1 else -1
-        distance = distance if distance <= 1 else 1
         return distance
     return 0
 
 
-def edit_quality(title_string, revision_i, revision_j):
-    return ((edit_distance(title_string, revision_i['parentid'], revision_j['revid'])
-            - edit_distance(title_string, revision_i['revid'], revision_j['revid']))
-            / edit_distance(title_string, revision_i['parentid'], revision_i['revid']))
+def edit_quality(title_object, revision_i, revision_j):
+    numerator = (edit_distance(title_object, revision_i['parentid'], revision_j['revid'])
+                 - edit_distance(title_object, revision_i['revid'], revision_j['revid']))
+
+    denominator = edit_distance(title_object, revision_i['parentid'], revision_i['revid'])
+
+    val = numerator if denominator == 0 or numerator == 0 else numerator / denominator
+
+    return -1 if val < 0 else 1  # must be one of[-1, 1]
 
 
 def get_contributing_authors(arg_tuple):
     global minimum_authors, minimum_contribution_pct
     title_object, title_revs = arg_tuple
-    title = title_object['title']
-    edit_longevity = dict()
-    top_authors_to_contrib = []
+    top_authors = []
     try:
-        for i in range(1, len(title_revs)-1):
+        for i in range(1, len(title_revs)):
             prev_rev = title_revs[i-1]
             curr_rev = title_revs[i]
             if 'revid' not in curr_rev or 'revid' not in prev_rev:
                 continue
 
             otherrevs = [title_revs[j] for j in range(i+1, len(title_revs[i+1:i+11]))]
-            non_author_revs = filter(lambda x: x.get('userid', 0) != curr_rev.get('userid', 0), otherrevs)
-            average_edit_quality = (
-                sum(
-                    [edit_distance(title, curr_rev['revid'], otherrev['revid'])
-                     for otherrev in non_author_revs]
-                )
-                * max([1, len(set([non_author_rev.get('userid', 0) for non_author_rev in non_author_revs]))])
-            )
-            edit_longevity[curr_rev['revid']] = (average_edit_quality
-                                                 * edit_distance(title, prev_rev['revid'], curr_rev['revid']))
-        authors = list(set([title_rev.get('userid', 0) for title_rev in title_revs]))
-        title_contribs = {}
-        for author in authors:
-            title_contribs[author] = sum([edit_longevity[title_rev['revid']] for title_rev in title_revs
-                                          if title_rev.get('userid', 0) == author and title_rev['revid'] in edit_longevity])
+            non_author_revs = filter(lambda x: x.get('user', '') != curr_rev.get('user', ''), otherrevs)
+            avg_edit_qty = (sum([edit_quality(title_object, curr_rev, otherrev) for otherrev in non_author_revs])
+                            / max(1, len(set([non_author_rev.get('user', '') for non_author_rev in non_author_revs]))))
+            curr_rev['edit_longevity'] = (avg_edit_qty
+                                          * edit_distance(title_object, prev_rev['revid'], curr_rev['revid']))
 
-        all_contribs_sum = sum(title_contribs.values())
-        top_author_pct = 0
+        authors = filter(lambda x: x['userid'] != 0 and x['user'] != '',
+                         dict([(title_rev.get('userid', 0),
+                               {'userid': title_rev.get('userid', 0), 'user': title_rev.get('user', '')}
+                                ) for title_rev in title_revs]).values()
+                         )
+
         for author in authors:
-            if author != 0 and title_contribs[author] > 0:
-                author_contrib_pct = float(title_contribs[author])/all_contribs_sum
-                top_authors_to_contrib += [(author, author_contrib_pct)]
-                top_author_pct += author_contrib_pct
-            if len(top_authors_to_contrib) >= minimum_authors and top_author_pct >= minimum_contribution_pct:
+            author['contribs'] = sum([title_rev['edit_longevity'] for title_rev in title_revs
+                                      if title_rev.get('userid', 0) == author.get('userid', 0)
+                                      and 'edit_longevity' in title_rev and title_rev['edit_longevity'] > 0])
+
+        authors = filter(lambda x: x.get('contribs', 0) > 0, authors)
+
+        all_contribs_sum = sum([a['contribs'] for a in authors])
+
+        for author in authors:
+            author['contrib_pct'] = author['contribs']/all_contribs_sum
+
+        for author in sorted(authors, key=lambda x: x['contrib_pct'], reverse=True):
+            if 'user' not in author:
+                continue
+            if author['contrib_pct'] < minimum_contribution_pct and len(top_authors) >= minimum_authors:
                 break
+            top_authors += [author]
+
     except IndexError:
         print title, sys.exc_info()
 
-    return title, sorted(top_authors_to_contrib, key=lambda x: x[1], reverse=True)
+    return title_object['title'], top_authors
 
 
 def links_for_page(title_object, plcontinue=None):
@@ -170,15 +197,15 @@ def get_pagerank(titles):
 def author_centrality(titles_to_authors):
     author_graph = digraph()
     author_graph.add_nodes(map(lambda x: "title_%s" % x, titles_to_authors.keys()))
-    author_graph.add_nodes(list(set(['author_%s' % author for value in titles_to_authors.values()
-                                     for author, contrib in value])))
+    author_graph.add_nodes(list(set(['author_%s' % author['user']
+                                     for title in titles_to_authors
+                                     for author in titles_to_authors[title]])))
     map(author_graph.add_edge,
-        [('title_%s' % title, 'author_%s' % author)
+        [('title_%s' % title, 'author_%s' % author['user'])
          for title in titles_to_authors
-         for author, contrib in titles_to_authors[title]])
+         for author in titles_to_authors[title]])
 
-    # casting to int here but maybe want to use username instead?
-    return dict([(int('_'.join(item[0].split('_')[1:])), item[1])
+    return dict([('_'.join(item[0].split('_')[1:]), item[1])
                  for item in pagerank(author_graph).items() if item[0].startswith('author_')])
 
 
@@ -187,10 +214,12 @@ try:
 except NotImplementedError:
     cpus = 2   # arbitrary default
 
+cpus = 32
+
 wiki_id = sys.argv[1]
 test_run = len(sys.argv) >= 3
 minimum_authors = 5
-minimum_contribution_pct = 0.7
+minimum_contribution_pct = 0.01
 
 aplimit = 500 if not test_run else 10
 
@@ -203,15 +232,30 @@ all_titles = get_all_titles(api_url, aplimit=aplimit)
 print "Got %d titles" % len(all_titles)
 
 pool = multiprocessing.Pool(processes=cpus)
-all_revisions = dict(pool.map(get_all_revisions, all_titles))
 
-print "%d Revisions" % sum([len(rev) for rev in all_revisions])
+all_revisions = []
+r = pool.map_async(get_all_revisions, all_titles, callback=all_revisions.extend)
+r.wait()
+print "%d Revisions" % sum([len(revs) for title, revs in all_revisions])
+all_revisions = dict(all_revisions)
 
-top_authors = dict(pool.map(get_contributing_authors,
-                            [(title_obj, all_revisions[title_obj['title']]) for title_obj in all_titles]))
+title_top_authors = {}
 
-centralities = author_centrality(dict(top_authors))
+r = pool.map_async(get_contributing_authors,
+                   [(title_obj, all_revisions[title_obj['title']]) for title_obj in all_titles],
+                   callback=title_top_authors.update)
+r.wait()
+print title_top_authors
+
+centralities = author_centrality(title_top_authors)
+
+centrality_scaler = MinMaxScaler(centralities.values())
+contribs_scaler = MinMaxScaler([author['contribs']
+                                for title in title_top_authors
+                                for author in title_top_authors[title]])
 
 # this com_qscore_pr, the best metric per Qin and Cunningham
-print [(title['title'], sum([score * centralities[author] for author, score in top_authors[title['title']]]))
-       for title in all_titles]
+print [(title,
+        sum([contribs_scaler.scale(author['contribs']) * centrality_scaler.scale(centralities[author['user']])
+             for author in title_top_authors[title]]))
+       for title in title_top_authors]
