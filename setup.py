ff03917d424ff524abc196e21118374d5f3b9374
Robert Elwell <robert@wikia-inc.com>
2014-02-26 09:36:15 -0800

    update dependencies

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author_email = "robert@wikia-inc.com",
     description = "Library for identifying authorship quality in a revision-based system",
     license = "Other",
-    packages = ["wikia_authority", "wikia_authority.drivers"],
-    depends = [ "requests", "lxml", "cssselect", "python-graph-core"]
+    packages = ["wikia_authority"],
+    depends = [ "requests", "lxml", "cssselect", "python-graph-core", "xlrd", "xlwt"]
     )
