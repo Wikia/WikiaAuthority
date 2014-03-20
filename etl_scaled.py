27be1618d379cd53d8b0c68e30a059e8a3b677e9
Robert Elwell <robert@wikia-inc.com>
2014-03-20 12:19:10 -0700

    update etl_scaled to be more configurable

--- etl_scaled.py
+++ etl_scaled.py
@@ -1,15 +1,7 @@
 import subprocess
 import sys
-import random
+from argparse import ArgumentParser, FileType
 from boto import connect_s3
-
-c = connect_s3()
-b = c.get_bucket('nlp-data')
-
-failed_events = open('/var/log/authority_failed.txt', 'a')
-
-lines = [line for line in open(sys.argv[1])]
-random.shuffle(lines)
 
 
 class Unbuffered:
@@ -25,18 +17,39 @@ class Unbuffered:
         return getattr(self.stream, attr)
 
 
-sys.stdout = Unbuffered(sys.stdout)
+def get_args():
+    ap = ArgumentParser()
+    ap.add_argument('--infile', dest='infile', type=FileType('r'))
+    ap.add_argument('--s3file', dest='s3file')
+    ap.add_argument('--overwrite', dest='overwrite', action='store_true', default=False)
+    return ap.parse_args()
 
 
-for line in lines:
-    key = b.get_key(key_name='service_responses/%s/WikiAuthorityService.get' % line.strip())
-    if key is not None and key.exists():
-        print "Key exists for", line.strip()
-        continue
-    print "Wiki ", line.strip()
-    try:
-        print subprocess.call("python api_to_database.py --wiki-id=%s --processes=64" % line.strip(), shell=True)
-    except Exception as e:
-        print e
-        failed_events.write(line)
+def main():
+    sys.stdout = Unbuffered(sys.stdout)
+    bucket = connect_s3().get_bucket('nlp-data')
+    failed_events = open('/var/log/authority_failed.txt', 'a')
 
+    args = get_args()
+    if args.s3file:
+        fname = args.s3file.split('/')[-1]
+        bucket.get_key(args.s3file).get_file(open(fname, 'w'))
+        fl = open(fname, 'r')
+    else:
+        fl = args.infile
+
+    for line in fl:
+        key = bucket.get_key(key_name='service_responses/%s/WikiAuthorityService.get' % line.strip())
+        if (not args.overwrite) and (key is not None and key.exists()):
+            print "Key exists for", line.strip()
+            continue
+        print "Wiki ", line.strip()
+        try:
+            print subprocess.call("python api_to_database.py --wiki-id=%s --processes=64" % line.strip(), shell=True)
+        except Exception as e:
+            print e
+            failed_events.write(line)
+
+
+if __name__ == '__main__':
+    main()
