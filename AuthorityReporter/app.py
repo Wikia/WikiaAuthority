526d659829d66e9a8fa4ce84072bb424b2db2bf8
Robert Elwell <robert@wikia-inc.com>
2014-04-08 15:28:39 -0700

    reuse old app

--- AuthorityReporter/app.py
+++ AuthorityReporter/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 import requests
 import argparse
+import json
+from wikia_dstk.authority import add_db_arguments, get_db_and_cursor
 from nlp_services.authority import WikiAuthorityService, PageAuthorityService
 from nlp_services.authority import WikiAuthorTopicAuthorityService, WikiAuthorsToIdsService
 from nlp_services.authority import WikiTopicsToAuthorityService
@@ -11,17 +13,17 @@ from multiprocessing import Pool
 use_caching()
 
 
-def update_top_page(args):
-    wiki_id, top_page = args
-    no_image_url = ("http://slot1.images.wikia.nocookie.net/__cb62407/"
-                    + "common/extensions/wikia/Search/images/wiki_image_placeholder.png")
-    if top_page.get('thumbnail') is None:
-        top_page['thumbnail'] = no_image_url
-    top_page['entities'] = CombinedWikiPageEntitiesService().get_value(wiki_id+'_'+str(top_page['id']))
-    top_page['authorities'] = PageAuthorityService().get_value(wiki_id+'_'+str(top_page['id']))
-    for author in top_page['authorities']:
-        author['contrib_pct'] = "%.2f%%" % (author['contrib_pct'] * 100.0)
-    top_page['authority'] = "%.2f" % (100.0 * top_page['authority'])
+def update_top_page(arg_tuple):
+    wiki_id, top_page = arg_tuple
+    no_image_url = (u"http://slot1.images.wikia.nocookie.net/__cb62407/"
+                    + u"common/extensions/wikia/Search/images/wiki_image_placeholder.png")
+    if top_page.get(u'thumbnail') is None:
+        top_page[u'thumbnail'] = no_image_url
+    top_page[u'entities'] = CombinedWikiPageEntitiesService().get_value(wiki_id+'_'+str(top_page['id']))
+    top_page[u'authorities'] = PageAuthorityService().get_value(wiki_id+'_'+str(top_page['id']))
+    for author in top_page[u'authorities']:
+        author[u'contrib_pct'] = u"%.2f%%" % (author[u'contrib_pct'] * 100.0)
+    top_page[u'authority'] = u"%.2f" % (100.0 * top_page[u'authority'])
 
     return top_page
 
@@ -29,7 +31,6 @@ def update_top_page(args):
 app = Flask(__name__)
 
 POOL = Pool(processes=20)
-SOLR_URL = 'http://search-s10:8983/main/'
 WIKI_ID = None
 WIKI_AUTHORITY_DATA = None
 WIKI_API_DATA = None
@@ -40,30 +41,30 @@ def configure_wiki_id(wiki_id):
     if WIKI_ID != wiki_id:
         WIKI_AUTHORITY_DATA = WikiAuthorityService().get_value(wiki_id)
         WIKI_ID = wiki_id
-        WIKI_API_DATA = requests.get('http://www.wikia.com/api/v1/Wikis/Details',
+        WIKI_API_DATA = requests.get(u'http://www.wikia.com/api/v1/Wikis/Details',
                                      params=dict(ids=WIKI_ID)).json()['items'][wiki_id]
 
 
-@app.route('/<wiki_id>/topics/')
+@app.route(u'/<wiki_id>/topics/')
 def topics(wiki_id):
     global WIKI_ID, WIKI_API_DATA, WIKI_AUTHORITY_DATA, POOL
     configure_wiki_id(wiki_id)
 
-    topics = WikiTopicsToAuthorityService().get_value(wiki_id)
-    top_topics = [dict(topic=topic, authority=data['authority'], authors=data['authors'])
-                  for topic, data in sorted(topics, key=lambda x: x[1]['authority'], reverse=True)[:10]]
+    wiki_topics = WikiTopicsToAuthorityService().get_value(wiki_id)
+    top_topics = [dict(topic=topic, authority=data[u'authority'], authors=data[u'authors'])
+                  for topic, data in sorted(wiki_topics, key=lambda x: x[1][u'authority'], reverse=True)[:10]]
 
-    return render_template('topics.html', topics=top_topics, wiki_api_data=WIKI_API_DATA)
+    return render_template(u'topics.html', topics=top_topics, wiki_api_data=WIKI_API_DATA)
 
 
-@app.route('/<wiki_id>/authors/')
+@app.route(u'/<wiki_id>/authors/')
 def authors(wiki_id):
     global WIKI_ID, WIKI_API_DATA, WIKI_AUTHORITY_DATA, POOL
     configure_wiki_id(wiki_id)
 
     topic_authority_data = WikiAuthorTopicAuthorityService().get_value(wiki_id)
 
-    authors_to_topics = sorted(topic_authority_data['weighted'].items(),
+    authors_to_topics = sorted(topic_authority_data[u'weighted'].items(),
                                key=lambda y: sum(y[1].values()),
                                reverse=True)[:10]
 
@@ -74,21 +75,34 @@ def authors(wiki_id):
                                      topics=sorted(x[1].items(), key=lambda z: z[1], reverse=True)[:10]))
                          for x in authors_to_topics])
 
-    user_api_data = requests.get(WIKI_API_DATA['url']+'/api/v1/User/Details',
-                                 params={'ids': ','.join([str(a2ids[user]) for user, contribs in authors_to_topics]),
-                                 'format': 'json'}).json()['items']
+    user_api_data = requests.get(WIKI_API_DATA[u'url']+u'/api/v1/User/Details',
+                                 params={u'ids': u','.join([str(a2ids[user]) for user, contribs in authors_to_topics]),
+                                         u'format': u'json'}).json()[u'items']
 
     for user_data in user_api_data:
-        authors_dict[user_data['name']].update(user_data)
-        authors_dict[user_data['name']]['url'] = authors_dict[user_data['name']]['url'][1:]
+        authors_dict[user_data[u'name']].update(user_data)
+        authors_dict[user_data[u'name']][u'url'] = authors_dict[user_data[u'name']][u'url'][1:]
 
-    author_objects = sorted(authors_dict.values(), key=lambda z: z['total_authority'], reverse=True)
+    author_objects = sorted(authors_dict.values(), key=lambda z: z[u'total_authority'], reverse=True)
 
-    return render_template('authors.html', authors=author_objects, wiki_api_data=WIKI_API_DATA)
+    return render_template(u'authors.html', authors=author_objects, wiki_api_data=WIKI_API_DATA)
 
 
-@app.route('/<wiki_id>/<page>/')
-def index(wiki_id, page):
+
+@app.route(u'/wiki_autocomplete.js')
+def wiki_autocomplete():
+    global args
+    db, cursor = get_db_and_cursor(args)
+    cursor.execute(u"""SELECT wiki_id, title FROM wikis""")
+    wikis = dict([(row[1], row[0]) for row in cursor.fetchall()])
+    return Response(u"var wikis = %s;" % json.dumps(wikis),
+                    mimetype=u"application/javascript",
+                    content_type=u"application/javascript")
+
+
+
+@app.route(u'/<wiki_id>/<page>/')
+def page_index(wiki_id, page):
     page = int(page)
     global WIKI_ID, WIKI_API_DATA, WIKI_AUTHORITY_DATA, POOL
     configure_wiki_id(wiki_id)
@@ -96,8 +110,8 @@ def index(wiki_id, page):
     top_docs = sorted(WIKI_AUTHORITY_DATA.items(), key=lambda z: z[1], reverse=True)
     top_page_tups = [(tup[0].split('_')[-1], tup[1]) for tup in top_docs[(page-1)*10:page*10]]
 
-    page_api_data = requests.get(WIKI_API_DATA['url'].split('/wiki')[0]+'/api/v1/Articles/Details',
-                                 params={'ids': ','.join([x[0] for x in top_page_tups])}).json()['items']
+    page_api_data = requests.get(WIKI_API_DATA[u'url'].split(u'/wiki')[0]+u'/api/v1/Articles/Details',
+                                 params={u'ids': ','.join([x[0] for x in top_page_tups])}).json()[u'items']
 
     top_pages = []
     for tup in top_page_tups:
@@ -109,23 +123,28 @@ def index(wiki_id, page):
     r = POOL.map_async(update_top_page, [(str(wiki_id), page) for page in top_pages])
     r.wait()
 
-    if WIKI_API_DATA['url'].endswith('/'):
-        WIKI_API_DATA['url'] = WIKI_API_DATA['url'][:-1]
-    return render_template('index.html', docs=r.get(), wiki_api_data=WIKI_API_DATA)
+    if WIKI_API_DATA[u'url'].endswith(u'/'):
+        WIKI_API_DATA[u'url'] = WIKI_API_DATA[u'url'][:-1]
+    return render_template(u'index.html', docs=r.get(), wiki_api_data=WIKI_API_DATA)
+
+
+@app.route(u'/')
+def index():
+    return render_template(u'v2_index.html')
 
 
 def main():
     global app
-    parser = argparse.ArgumentParser(description='Authority Flask App')
-    parser.add_argument('--host', dest='host', action='store', default='0.0.0.0',
-                        help="App host")
-    parser.add_argument('--port', dest='port', action='store', default=5000, type=int,
-                        help="App port")
+    parser = add_db_arguments(argparse.ArgumentParser(description=u'Authority Flask App'))
+    parser.add_argument(u'--app-host', dest=u'app_host', action=u'store', default=u'0.0.0.0',
+                        help=u"App host")
+    parser.add_argument(u'--app-port', dest=u'app_port', action=u'store', default=5000, type=int,
+                        help=u"App port")
     options = parser.parse_args()
 
     app.debug = True
     app.run(host=options.host, port=options.port)
 
 
-if __name__ == '__main__':
+if __name__ == u'__main__':
     main()
--- AuthorityReporter/v2app.py
+++ AuthorityReporter/v2app.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from wikia_dstk.authority import add_db_arguments, get_db_and_cursor
+import json
 from flask import Flask, render_template, Response
 import json
 import requests
@@ -23,23 +24,35 @@ def main():
     app.run(host=args.app_host, port=args.app_port)
 
 
-@app.route(u'/wiki_autocomplete.js')
-def wiki_autocomplete():
+
+@app.route(u'/wiki/<wiki_id>/topics/')
+def wiki_topics(wiki_id):
     global args
     db, cursor = get_db_and_cursor(args)
-    cursor.execute(u"""SELECT wiki_id, title FROM wikis""")
-    wikis = dict([(row[1], row[0]) for row in cursor.fetchall()])
-    return Response(u"var wikis = %s;" % json.dumps(wikis),
-                    mimetype=u"application/javascript",
-                    content_type=u"application/javascript")
+    cursor.execute(u"""SELECT topics.topic_id, topics.name, SUM(articles.local_authority) AS topic_authority
+                       FROM articles
+                       INNER JOIN articles_topics
+                            ON articles.wiki_id = %s AND articles_topics.wiki_id = %s
+                            AND articles.article_id = articles_topics.article_id
+                       INNER JOIN topics ON articles_topics.topic_id = topics.topic_id
+                       GROUP BY topics.topic_id
+                       ORDER BY topic_authority DESC
+                       """ % (wiki_id, wiki_id))
+    id_to_authority = [(row[0], row[1], row[2]) for row in cursor.fetchall()]
+    cursor.execute(u"""SELECT title, url FROM wikis WHERE wiki_id = %s""" % wiki_id)
+    (wiki_title, wiki_url,) = cursor.fetchone()
+    response = requests.get(wiki_url+u'api/v1/Articles/Details',
+                            params=dict(ids=u','.join([str(a[0]) for a in id_to_authority])))
+
+    page_data = dict(response.json().get(u'items', {}))
+    pages = []
+    for pageid, authority in id_to_authority:
+        pages.append(dict(authority=authority, pageid=pageid, **page_data.get(str(pageid), {})))
+    return render_template(u'v2_wiki_articles.html',
+                           pages=pages, wiki_url=wiki_url, wiki_title=wiki_title, wiki_id=wiki_id)
 
 
-@app.route(u'/wiki/<wiki_id>/articles/topics/')
-def wiki_users(wiki_id):
-    pass
-
-
-@app.route(u'/wiki/<wiki_id>/articles/topics/')
+@app.route(u'/wiki/<wiki_id>/articles/')
 def wiki_articles(wiki_id):
     global args
     db, cursor = get_db_and_cursor(args)
