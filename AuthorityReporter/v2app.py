139dd640cf6a865453d05bb4644a2d8d5ebdc036
Robert Elwell <robert@wikia-inc.com>
2014-04-08 11:42:49 -0700

    ughh

--- AuthorityReporter/v2app.py
+++ AuthorityReporter/v2app.py
@@ -44,9 +44,9 @@ def wiki(wiki_id):
     cursor.execute(u"""SELECT title, url FROM wikis WHERE wiki_id = %s""" % wiki_id)
     (wiki_title, wiki_url,) = cursor.fetchone()
     response = requests.get(wiki_url+u'api/v1/Articles/Details',
-                            params=dict(ids=u','.join([str(a[0]) for a in id_to_authority]))).json()
+                            params=dict(ids=u','.join([str(a[0]) for a in id_to_authority])))
     print response.url
-    page_data = dict(response.get(u'items', {}))
+    page_data = dict(response.json().get(u'items', {}))
     pages = []
     for pageid, authority in id_to_authority:
         pages.append(dict(authority=authority, pageid=pageid, **page_data.get(pageid, {})))
